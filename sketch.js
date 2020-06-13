c816c34ec0cb514c0897b908d0a9c7155d26835b
AtharvBatra <atharv.batra@outlook.com>
2020-06-13 15:09:48 +0530

    ...

--- sketch.js
+++ sketch.js
@@ -1,29 +1,17 @@
-var ball;
-
+var canvas,backgroundImage;
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-}
 
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+  canvas=createCanvas(400,400);
+  database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
 }
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function draw()
+{
+  
 }
